45400a7a037ae6a2c6da4b8d6ba8b7d28b293996
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-14 17:32:58 +0530

    Finished up the webpage

--- src/App.jsx
+++ src/App.jsx
@@ -14,36 +14,42 @@ function App() {
         <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
           <div
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("red")}
             style={{ backgroundColor: "red" }}
           >
             Red
           </div>
           <div
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("green")}
             style={{ backgroundColor: "green" }}
           >
             Green
           </div>
           <div
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("blue")}
             style={{ backgroundColor: "blue" }}
           >
             Blue
           </div>
           <div
             className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            onClick={() => setColor("orange")}
             style={{ backgroundColor: "orange" }}
           >
             Orange
           </div>
           <div
             className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            onClick={() => setColor("yellow")}
             style={{ backgroundColor: "yellow" }}
           >
             Yellow
           </div>
           <div
             className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            onClick={() => setColor("pink")}
             style={{ backgroundColor: "pink" }}
           >
             Pink
