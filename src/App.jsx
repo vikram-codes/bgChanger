e56afc1181888fbfe48a37f3b07cfb060586ab70
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-14 16:57:42 +0530

    removed 8px margin from the app.css file

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,8 @@ function App() {
 
   return (
     <div
-      className="w-full fixed h-screen duration-200"
+      className="w-full h-screen
+       duration-200"
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
