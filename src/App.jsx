e658f3efcb631f4d13ad9c254f9c97879f8180d5
Vikram Saini <vikramjit.personal@gmail.com>
2023-10-14 17:11:38 +0530

    Added colors button at the button

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [color, setColor] = useState("olive");
+  const [color, setColor] = useState("green");
 
   return (
     <div
@@ -11,7 +11,44 @@ function App() {
       style={{ backgroundColor: color }}
     >
       <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        Hello Word
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl">
+          <div
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "red" }}
+          >
+            Red
+          </div>
+          <div
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "green" }}
+          >
+            Green
+          </div>
+          <div
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "blue" }}
+          >
+            Blue
+          </div>
+          <div
+            className="outline-none px-4 py-1 rounded-full text-white shadow-lg"
+            style={{ backgroundColor: "orange" }}
+          >
+            Orange
+          </div>
+          <div
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "yellow" }}
+          >
+            Yellow
+          </div>
+          <div
+            className="outline-none px-4 py-1 rounded-full text-black shadow-lg"
+            style={{ backgroundColor: "pink" }}
+          >
+            Pink
+          </div>
+        </div>
       </div>
     </div>
   );
